ee76eb92527de2ecc87935e0e2762cef2dbffed9
Marcel <marcelbastos12@hotmail.com>
2018-07-10 00:28:00 -0300

    javascriptcomeco

---
+++ index.js
@@ -0,0 +1,4 @@
+alert("Welcome to your bank bro!");
+var deposit = prompt("Quanto vc quer depositar na sua linda conta gorducha hoje?")
+alert("Vc depositou: "+deposit+" na sua conta gorducha")
+console.log("Vc é um cara legalzão!")
