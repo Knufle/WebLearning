241db4735fd4ad5cb39d3d13a5f8efa227aa6ba9
Knufle <marcelbastos12@hotmail.com>
2018-07-15 12:01:59 -0300

    organizacao arquivos e funcoes javascript

---
+++ JS/jsfunctions.js
@@ -0,0 +1,21 @@
+function helloyou(name){
+  console.log("hello "+name);
+}
+
+function addNum(num1,num2) {
+  console.log(num1+num2);
+}
+
+function helloalguem(name="Roberto") {
+  console.log("Hello "+name);
+}
+
+function formal(name="Marcel",title="Sir") {
+  // console.log(title + " "+name);
+  return title + " "+name;
+}
+
+function vezescinco(numInput) {
+  var result = numInput * 5;
+  return result;
+}
